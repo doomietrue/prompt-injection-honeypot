d44516687b1c29caf1b6215620ac2e1fb7fd831c
doomietrue <doomietrue@users.noreply.github.com>
2026-01-31 00:39:55 +0100

    Test: add emoji to title

--- madpine/index.php
+++ madpine/index.php
@@ -58,7 +58,7 @@ $payloads = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Injection Honeypot - Educational Tool</title>
+    <title>🍯 Injection Honeypot - Educational Tool</title>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
